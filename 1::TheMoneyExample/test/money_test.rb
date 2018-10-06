344863263f94681b30f6f87f203d9ae51d9a39fb
katorie <katoriexxxkatorie@gmail.com>
2018-10-06 11:59:02 +0900

    4:Privacy

--- 1::TheMoneyExample/test/money_test.rb
+++ 1::TheMoneyExample/test/money_test.rb
@@ -4,15 +4,18 @@ require './lib/dollar'
 class MoneyTest < Minitest::Test
   def test_multiplication
     five = Dollar.new(5)
-    product = five.times(2)
-    assert_equal 10, product.amount
-
-    product = five.times(3)
-    assert_equal 15, product.amount
+    assert_equal Dollar.new(10), five.times(2)
+    assert_equal Dollar.new(15), five.times(3)
   end
 
   def test_equality
     assert Dollar.new(5).equals Dollar.new(5)
     refute Dollar.new(5).equals Dollar.new(6)
+  end
+end
+
+class Dollar
+  def ==(x)
+    x.instance_of?(Dollar) and @amount == x.amount
   end
 end
